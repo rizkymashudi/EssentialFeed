4aa2f7105685abc4835dc962dba187095d64a15c
rizky mashudi <v00029222@danamon.co.id>
2025-12-15 19:41:39 +0700

    chore: guarantee that we can load more than once

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,20 +8,30 @@
 import XCTest
 @testable import EssentialFeed
 
-class RemoteFeedLoaderTests: XCTest {
+class RemoteFeedLoaderTests: XCTestCase {
   func test_init_doesNotRequestDataFromURL() {
     let (_, client) = makeSUT()
     
     XCTAssertNil(client.requestedURL)
   }
   
-  func test_load_requestDataFromURL() {
+  func test_load_requestsDataFromURL() {
     let url = URL(string: "https://a-given-url.com")!
     let (sut, client) = makeSUT(url: url)
     
     sut.load()
     
     XCTAssertEqual(client.requestedURL, url)
+  }
+  
+  func test_loadTwice_requestsDataFromURLTwice() {
+    let url = URL(string: "https://a-given-url.com")!
+    let (sut, client) = makeSUT(url: url)
+    
+    sut.load()
+    sut.load()
+    
+    XCTAssertEqual(client.requestedURLs, [url, url])
   }
   
   private func makeSUT(
@@ -34,9 +44,11 @@ class RemoteFeedLoaderTests: XCTest {
   
   private class MockHTTPClient: HTTPClient {
     var requestedURL: URL?
+    var requestedURLs = [URL]()
     
     func get(from url: URL) {
       requestedURL = url
+      requestedURLs.append(url)
     }
   }
 }
