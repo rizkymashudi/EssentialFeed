455a7972df29cdd531188371742e5d568ea7f4bc
rizky mashudi <v00029222@danamon.co.id>
2025-12-30 14:25:16 +0700

    chore: add remote feed loader result type with success and failure cases

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -83,12 +83,12 @@ extension RemoteFeedLoaderTests {
     file: StaticString = #file,
     line: UInt = #line
   ) {
-    var capturedErrors = [RemoteFeedLoader.Error]()
-    sut.load { capturedErrors.append($0) }
+    var capturedResults = [RemoteFeedLoader.Result]()
+    sut.load { capturedResults.append($0) }
     
     action()
     
-    XCTAssertEqual(capturedErrors, [error], file: file, line: line)
+    XCTAssertEqual(capturedResults, [.failure(error)], file: file, line: line)
   }
   
   private class MockHTTPClient: HTTPClient {
