d378fffd21b4f842c8d33ba701e72fc30bce3690
rizky mashudi <v00029222@danamon.co.id>
2026-02-07 21:04:07 +0700

    chore: create URLProtocolStub intercept every network request instead of URL-based mocking since we dont want to ever perform a network request during a tests. Also, we want our assertions to be more precise about the error when they fail

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -17,8 +17,8 @@ class URLSessionHTTPClient {
   }
   
   func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-    let request = URLRequest(url: url)
-    session.dataTask(with: request) { _, _, error in
+    let url = URL(string: "http://wrong-url.com")!
+    session.dataTask(with: url) { _, _, error in
       if let error = error {
         completion(.failure(error))
         return
@@ -35,7 +35,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     URLProtocolStub.startInterceptingRequests()
     let url = URL(string: "http://any-url.com")!
     let error = NSError(domain: "Any error", code: 1)
-    URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
+    URLProtocolStub.stub(data: nil, response: nil, error: error)
     
     let sut = URLSessionHTTPClient()
     
@@ -62,7 +62,7 @@ class URLSessionHTTPClientTests: XCTestCase {
 // MARK: Helpers
 extension URLSessionHTTPClientTests {
   private class URLProtocolStub: URLProtocol {
-    private static var stubs = [URL: Stub]()
+    private static var stub: Stub?
     
     private struct Stub {
       let data: Data?
@@ -70,8 +70,8 @@ extension URLSessionHTTPClientTests {
       let error: Error?
     }
     
-    static func stub(url: URL, data: Data? = nil, response: URLResponse? = nil, error: Error?) {
-      stubs[url] = Stub(data: data, response: response, error: error)
+    static func stub( data: Data? = nil, response: URLResponse? = nil, error: Error?) {
+      stub = Stub(data: data, response: response, error: error)
     }
     
     static func startInterceptingRequests() {
@@ -80,12 +80,11 @@ extension URLSessionHTTPClientTests {
     
     static func stopInterceptingRequests() {
       URLProtocol.unregisterClass(URLProtocolStub.self)
-      stubs = [:]
+      stub = nil
     }
     
     override class func canInit(with request: URLRequest) -> Bool {
-      guard let url = request.url else { return false }
-      return URLProtocolStub.stubs[url] != nil
+      return true
     }
     
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
@@ -93,16 +92,15 @@ extension URLSessionHTTPClientTests {
     }
     
     override func startLoading() {
-      guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
-      if let data = stub.data {
+      if let data = URLProtocolStub.stub?.data {
         client?.urlProtocol(self, didLoad: data)
       }
       
-      if let response = stub.response {
+      if let response = URLProtocolStub.stub?.response {
         client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
       }
       
-      if let error = stub.error {
+      if let error = URLProtocolStub.stub?.error {
         client?.urlProtocol(self, didFailWithError: error)
       }
       
