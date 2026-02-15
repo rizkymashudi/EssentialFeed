d1c8e780d90d29b64e73e3135b44c1af91d263a5
eremism <rizky_mashudy@yahoo.co.id>
2026-02-15 13:59:55 +0700

    chore: does not delete upon `LocalFeedLoader` creation

---
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,28 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeed
+//
+//  Created by Rizky Mashudi on 15/02/26.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+  init(store: FeedStore) {
+    
+  }
+}
+
+class FeedStore {
+  var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+  
+  func test_init_doesNotDeleteCacheUponCreation() {
+    let store = FeedStore()
+    let _ = LocalFeedLoader(store: store)
+    
+    XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+  }
+}
